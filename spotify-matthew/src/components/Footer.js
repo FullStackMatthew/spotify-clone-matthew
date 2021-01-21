aca74bb4a49a81f828715981e54dbc5d4ba5de4b
FullStackMatthew <fullstackmatthew@gmail.com>
2021-01-20 19:09:35 -0500

    new project
    first commit

--- spotify-matthew/src/components/Footer.js
+++ spotify-matthew/src/components/Footer.js
@@ -1,18 +1,7 @@
 import React,{useEffect} from 'react'
 import "./styles/Footer.css"
-/*import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline"
-import SkipPreviousIcon from "@material-ui/icons/SkipPrevious"
-import SkipNextIcon from "@material-ui/icons/SkipNext"
-import ShuffleIcon from "@material-ui/icons/Shuffle"
-import RepeatIcon from "@material-ui/icons/Repeat"
-import VolumeDownIcon from "@material-ui/icons/VolumeDown"
-import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline"
-import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay"
-import { Grid, Slider } from "@material-ui/core"*/
 import { useDataLayerValue } from '../DataLayer'
 import SpotifyPlayer from 'react-spotify-web-playback'
-//npm i react-spotify-web-playback
-//https://github.com/gilbarbara/react-spotify-web-playback
 
 export const Footer =({spotify})=>{
     const [{play_uri, token, playing }, dispatch] = useDataLayerValue()
--- spotify-matthew/src/index.js
+++ spotify-matthew/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {DataLayer} from './DataLayer'//StateProvider
+import {DataLayer} from './DataLayer'
 import {initialState, reducer} from './reducer'
 
 ReactDOM.render(
